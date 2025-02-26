0aaf94757766d61e266c35a3632b0bfed8c62f4a
pratap-nani <pratap.nani@outlook.com>
2025-02-26 23:35:40 +0530

    Add files via upload

---
+++ LogHelper.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using System;
+
+namespace SampleFluentFTP
+{
+    public static class LogHelper
+    {
+        private static readonly ILog Log;
+
+        static LogHelper()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public static void Error(object msg)
+        {
+            Log.Error(msg);
+        }
+
+        public static void Error(object msg, Exception ex)
+        {
+            Log.Error(msg, ex);
+        }
+
+        public static void Error(Exception ex)
+        {
+            Log.Error(ex.Message, ex);
+        }
+
+        public static void Info(object msg)
+        {
+            Log.Info(msg);
+        }
+    }
+}
