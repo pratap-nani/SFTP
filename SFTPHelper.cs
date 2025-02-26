4888104010f50a30ce83d837f67b059b1d0e4a1b
pratap-nani <pratap.nani@outlook.com>
2025-02-27 00:11:15 +0530

    Add files via upload

---
+++ SFTPHelper.cs
@@ -0,0 +1,56 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+
+namespace SampleFluentFTP
+{
+    public class SFTPHelper : IDisposable
+    {
+        private SftpClient sftpClient;
+        public SFTPHelper(string host, string username, string password) 
+        {
+            try
+            {
+                sftpClient = new SftpClient(host, username, password);
+            }
+            catch (Exception e) {
+                LogHelper.Error(e.Message,e);
+            }
+            
+        }
+
+        public void UploadFile(string inputFilePath,string ftpFilePath)
+        {
+            if (!File.Exists(inputFilePath))
+            {
+                LogHelper.Info("File Not Found :" + inputFilePath);
+                return;
+            }
+
+            LogHelper.Info("Input File :" + inputFilePath);
+
+            string ftpFile = Path.Combine(ftpFilePath, Path.GetFileName(inputFilePath));
+
+
+            using (FileStream fs = File.OpenRead(inputFilePath))
+            {
+                LogHelper.Info("File Size :" + fs.Length);
+                LogHelper.Info("Upload File Path :" + ftpFilePath);
+                sftpClient.UploadFile(fs, ftpFile);
+                sftpClient.ServerIdentificationReceived += Client_ServerIdentificationReceived;
+
+                LogHelper.Info("File Uploaded Successfully.");
+            }
+        }
+
+        private static void Client_ServerIdentificationReceived(object sender, Renci.SshNet.Common.SshIdentificationEventArgs e)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            sftpClient?.Dispose();
+        }
+    }
+}
